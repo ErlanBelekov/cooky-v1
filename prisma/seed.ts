5c32304a786c9e000a7818c31d7b4ee9376dc60e
erlanbelekov <belekoverlanhehe@gmail.com>
2023-05-10 12:11:12 +0600

    update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,6 +5,14 @@ const prisma = new PrismaClient();
 async function seed() {
   const email = "erlan@remix.run";
 
+  await prisma.recipe.deleteMany({
+    where: {
+      user: {
+        email,
+      },
+    },
+  });
+
   // cleanup the existing database
   await prisma.user.delete({ where: { email } }).catch(() => {
     // no worries if it doesn't exist yet
@@ -16,10 +24,70 @@ async function seed() {
     },
   });
 
+  const veganTag = await prisma.tag.upsert({
+    where: {
+      id: 1,
+    },
+    create: {
+      title: "Vegan",
+    },
+    update: {},
+  });
+
+  const bakingTag = await prisma.tag.upsert({
+    where: {
+      id: 2,
+    },
+    create: {
+      title: "Baking",
+    },
+    update: {},
+  });
+
+  const irritableBowelSyndromeTag = await prisma.tag.upsert({
+    where: {
+      id: 3,
+    },
+    create: {
+      title: "Irritable bowel syndrome",
+    },
+    update: {},
+  });
+
+  await prisma.recipe.create({
+    data: {
+      title: "Roast beef",
+      userId: user.id,
+      images: {
+        set: [
+          "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/roast-beef-recipes-536cd86.jpg?quality=90&resize=440,400",
+        ],
+      },
+      tags: {
+        connect: [{ id: irritableBowelSyndromeTag.id }, { id: bakingTag.id }],
+      },
+    },
+  });
+
   await prisma.recipe.create({
     data: {
       title: "Shaverma",
       userId: user.id,
+      images: {
+        set: [
+          "https://eda.ru/img/eda/c620x415/s1.eda.ru/StaticContent/Photos/170215184917/200722222523/p_O.jpg",
+        ],
+      },
+      tags: {
+        connect: [
+          {
+            id: veganTag.id,
+          },
+          {
+            id: irritableBowelSyndromeTag.id,
+          },
+        ],
+      },
     },
   });
 
@@ -27,6 +95,16 @@ async function seed() {
     data: {
       title: "Pancakes",
       userId: user.id,
+      images: {
+        set: [
+          "https://static.onecms.io/wp-content/uploads/sites/43/2022/02/16/21014-Good-old-Fashioned-Pancakes-mfs_001.jpg",
+        ],
+      },
+      tags: {
+        connect: {
+          id: bakingTag.id,
+        },
+      },
     },
   });
 
