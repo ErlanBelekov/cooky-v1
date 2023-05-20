711c6f35a5d74a453288b545aa8a8bdc4b1f8c0b
erlanbelekov <belekoverlanhehe@gmail.com>
2023-05-21 00:20:31 +0600

    add DynamicRoutes

--- app/constants/routes.ts
+++ app/constants/routes.ts
@@ -1,5 +1,15 @@
 export enum Routes {
-  Explore = "/explore",
-  Diets = "/diets",
+  Explore = "/app/explore",
+  Diets = "/app/diets",
+  Settings = "/app/settings",
   Login = "/auth/login",
+  RecipeById = "/app/recipe/",
+}
+
+export class DynamicRoutes {
+  static shared = new DynamicRoutes();
+
+  public recipeById(recipeId: string): string {
+    return `/app/recipe/${recipeId}`;
+  }
 }
